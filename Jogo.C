b6dc5a2eb0f84ef9bb8b431eb2a22857a90bd0db
Giuseppe Oliveira <giuseppe_santos@Hotmail.com>
2023-04-01 17:17:47 -0300

    Update Project

---
+++ Jogo.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0;
+    int ebfirciy = 1;
+
+    do {
+        // comecar o jogo!!
+        
+    } while(!acertou && !enforcou);
+
+}
